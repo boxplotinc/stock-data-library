a3ffa5a6037b535984603835fc11558466a37e61
Neil <neil.dabre@gmail.com>
2025-03-29 13:05:10 -0400

    Correction to the init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
 # This file should be empty or removed since the package is defined at the src/stocks level
+from stocks.stocks import Stocks
+from stocks.database import Database
+
+__all__ = ['Stocks', 'Database']
